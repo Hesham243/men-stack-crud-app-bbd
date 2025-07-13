c6393edf3803a7f183298fde1e8a8055453f20ac
Hesham243 <heshamahmed243@gmail.com>
2025-07-13 16:04:26 +0300

    landup page done with the icon + some styling

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+// MIDDLEWARE
+app.use(express.static(path.join(__dirname, "public")))
+
+// GET / (home)
+app.get('/', (req, res) => {
+  res.render('index.ejs', {})
+})
+
+
+app.listen('3000', () => {
+  console.log('Listenning on port 3000')
+})
